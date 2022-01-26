d7cbf2682c8ccb06230349a1be75e8c487f07c0f
lemon <806416409@qq.com>
2022-01-26 13:56:43 +0800

    delete out

--- out/production/每日一题/Day07/Offer11.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def minArray(self, numbers):
-        """
-        :type numbers: List[int]
-        :rtype: int
-        """
-        numbers.sort()
-        return numbers[0]
